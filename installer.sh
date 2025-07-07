a86172fe500738fa2743994560457a0d7ac9a50d
loolkkekr <98670575+loolkkekr@users.noreply.github.com>
2025-07-07 03:40:07 +0300

    1.0-alpha1

--- installer.sh
+++ installer.sh
@@ -1,130 +1,201 @@
-#!/bin/bash
-HOME="/home/container"
-HOMEA="$HOME/linux/.apt"
-STAR1="$HOMEA/lib:$HOMEA/usr/lib:$HOMEA/var/lib:$HOMEA/usr/lib/x86_64-linux-gnu:$HOMEA/lib/x86_64-linux-gnu:$HOMEA/lib:$HOMEA/usr/lib/sudo"
-STAR2="$HOMEA/usr/include/x86_64-linux-gnu:$HOMEA/usr/include/x86_64-linux-gnu/bits:$HOMEA/usr/include/x86_64-linux-gnu/gnu"
-STAR3="$HOMEA/usr/share/lintian/overrides/:$HOMEA/usr/src/glibc/debian/:$HOMEA/usr/src/glibc/debian/debhelper.in:$HOMEA/usr/lib/mono"
-STAR4="$HOMEA/usr/src/glibc/debian/control.in:$HOMEA/usr/lib/x86_64-linux-gnu/libcanberra-0.30:$HOMEA/usr/lib/x86_64-linux-gnu/libgtk2.0-0"
-STAR5="$HOMEA/usr/lib/x86_64-linux-gnu/gtk-2.0/modules:$HOMEA/usr/lib/x86_64-linux-gnu/gtk-2.0/2.10.0/immodules:$HOMEA/usr/lib/x86_64-linux-gnu/gtk-2.0/2.10.0/printbackends"
-STAR6="$HOMEA/usr/lib/x86_64-linux-gnu/samba/:$HOMEA/usr/lib/x86_64-linux-gnu/pulseaudio:$HOMEA/usr/lib/x86_64-linux-gnu/blas:$HOMEA/usr/lib/x86_64-linux-gnu/blis-serial"
-STAR7="$HOMEA/usr/lib/x86_64-linux-gnu/blis-openmp:$HOMEA/usr/lib/x86_64-linux-gnu/atlas:$HOMEA/usr/lib/x86_64-linux-gnu/tracker-miners-2.0:$HOMEA/usr/lib/x86_64-linux-gnu/tracker-2.0:$HOMEA/usr/lib/x86_64-linux-gnu/lapack:$HOMEA/usr/lib/x86_64-linux-gnu/gedit"
-STARALL="$STAR1:$STAR2:$STAR3:$STAR4:$STAR5:$STAR6:$STAR7"
-export LD_LIBRARY_PATH=$STARALL
-export PATH="/bin:/usr/bin:/usr/local/bin:/sbin:$HOMEA/bin:$HOMEA/usr/bin:$HOMEA/sbin:$HOMEA/usr/sbin:$HOMEA/etc/init.d:$PATH"
-export BUILD_DIR=$HOMEA
+#!/bin/sh
 
-bold=$(echo -en "\e[1m")
-nc=$(echo -en "\e[0m")
-lightblue=$(echo -en "\e[94m")
-lightgreen=$(echo -en "\e[92m")
+ROOTFS_DIR=/home/container
 
-echo "
-${bold}${lightgreen}========================================================================
-                                                                                                  
-${bold}${lightblue}@@@@@@@   @@@@@@@  @@@@@@@@  @@@@@@@    @@@@@@      @@@  @@@  @@@@@@@@@@
-${bold}${lightblue}@@@@@@@@  @@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@     @@@  @@@  @@@@@@@@@@@    
-${bold}${lightblue}@@!  @@@    @@!    @@!       @@!  @@@  @@!  @@@     @@!  @@@  @@! @@! @@!    
-${bold}${lightblue}!@!  @!@    !@!    !@!       !@!  @!@  !@!  @!@     !@!  @!@  !@! !@! !@!     
-${bold}${lightblue}@!@@!@!     @!!    @!!!:!    @!@!!@!   @!@  !@!     @!@  !@!  @!! !!@ @!@      
-${bold}${lightblue}!!@!!!      !!!    !!!!!:    !!@!@!    !@!  !!!     !@!  !!!  !@!   ! !@!        
-${bold}${lightblue}!!:         !!:    !!:       !!: :!!   !!:  !!!     :!:  !!:  !!:     !!:        
-${bold}${lightblue}:!:         :!:    :!:       :!:  !:!  :!:  !:!      ::!!:!   :!:     :!:            
-${bold}${lightblue} ::          ::     :: ::::  ::   :::  ::::: ::       ::::    :::     ::        
-${bold}${lightblue} :           :     : :: ::    :   : :   : :  :         :       :      :          
-                                                                                                  
-                                                                                                                
-${bold}${lightgreen}========================================================================
- "
- 
-echo "${nc}"
+# =========================================================================================
+# Логика установки при первом запуске
+# =========================================================================================
+if [ ! -e $ROOTFS_DIR/.installed ]; then
+    # Очищаем экран для меню выбора
+    clear
 
-if [[ -f "./installed" ]]; then
-    echo "${bold}${lightgreen}==> Started ${lightblue}Container${lightgreen} <=="
-    function runcmd1 {
-        printf "${bold}${lightgreen}Default${nc}@${lightblue}Container${nc}:~ "
-        read -r cmdtorun
-        ./libraries/proot -S . /bin/bash -c "$cmdtorun"
-        runcmd
-    }
-    function runcmd {
-        printf "${bold}${lightgreen}Default${nc}@${lightblue}Container${nc}:~ "
-        read -r cmdtorun
-        ./libraries/proot -S . /bin/bash -c "$cmdtorun"
-        runcmd1
-    }
-    runcmd
-else
-    echo "Downloading files for application"
-    curl -sSLo ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip >/dev/null 2>err.log
-    echo -ne '#                   (5%)\r'
-    curl -sSLo files.zip https://github.com/RealTriassic/Ptero-VM-JAR/releases/download/latest/files.zip >/dev/null 2>err.log
-    echo -ne '##                  (10%)\r'
-    curl -sSLo unzip https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/unzip >/dev/null 2>err.log
-    echo -ne '####                (20%)\r'
-    curl -sSLo gotty https://raw.githubusercontent.com/afnan007a/Replit-Vm/main/gotty >/dev/null 2>err.log
-    echo -ne '#####               (25%)\r'
-    chmod +x unzip >/dev/null 2>err.log
-    export PATH="/bin:/usr/bin:/usr/local/bin:/sbin:$HOMEA/bin:$HOMEA/usr/bin:$HOMEA/sbin:$HOMEA/usr/sbin:$HOMEA/etc/init.d:$PATH"
-    ./unzip ngrok.zip >/dev/null 2>err.log
-    echo -ne '######               (30%)\r'
-    ./unzip files.zip >/dev/null 2>err.log
-    echo -ne '#######              (35%)\r'
-    ./unzip root.zip
-    tar -xf root.tar.gz >/dev/null 2>err.log
-    echo -ne '########             (40%)\r'
-    chmod +x ./libraries/proot >/dev/null 2>err.log
-    echo -ne '#########            (45%)\r'
-    chmod +x ngrok >/dev/null 2>err.log
-    echo -ne '##########           (50%)\r'
-    chmod +x gotty >/dev/null 2>err.log
-    echo -ne '###########          (55%)\r'
-    rm -rf files.zip >/dev/null 2>err.log
-    rm -rf root.zip >/dev/null 2>err.log
-    rm -rf root.tar.gz >/dev/null 2>err.log
-    rm -rf ngrok.zip >/dev/null 2>err.log
-    echo -ne '############         (60%)\r'
-
-    cmds=("mv gotty /usr/bin/" "mv unzip /usr/bin/" "mv ngrok /usr/bin/" "apt-get update" "apt-get -y upgrade" "apt-get -y install sudo curl wget hwloc htop nano neofetch python3" "curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py")
-
-    for cmd in "${cmds[@]}"; do
-        ./libraries/proot -S . /bin/bash -c "$cmd >/dev/null 2>err.log"
-    done
-    echo -ne '####################(100%)\r'
-    echo -ne '\n'
-    touch installed
+    # Переменные для URL и сообщения
+    ROOTFS_URL=""
+    PLACEHOLDER_MSG=""
+    ARCH=$(uname -m)
+    # --- Меню выбора дистрибутива ---
+    echo "Выберите дистрибутив для установки:"
+    echo "[1] Ubuntu"
+    echo "[2] Alpine"
     
-    echo "
-${bold}${lightgreen}========================================================================
-                                                                                                  
-${bold}${lightblue}@@@@@@@   @@@@@@@  @@@@@@@@  @@@@@@@    @@@@@@      @@@  @@@  @@@@@@@@@@
-${bold}${lightblue}@@@@@@@@  @@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@     @@@  @@@  @@@@@@@@@@@    
-${bold}${lightblue}@@!  @@@    @@!    @@!       @@!  @@@  @@!  @@@     @@!  @@@  @@! @@! @@!    
-${bold}${lightblue}!@!  @!@    !@!    !@!       !@!  @!@  !@!  @!@     !@!  @!@  !@! !@! !@!     
-${bold}${lightblue}@!@@!@!     @!!    @!!!:!    @!@!!@!   @!@  !@!     @!@  !@!  @!! !!@ @!@      
-${bold}${lightblue}!!@!!!      !!!    !!!!!:    !!@!@!    !@!  !!!     !@!  !!!  !@!   ! !@!        
-${bold}${lightblue}!!:         !!:    !!:       !!: :!!   !!:  !!!     :!:  !!:  !!:     !!:        
-${bold}${lightblue}:!:         :!:    :!:       :!:  !:!  :!:  !:!      ::!!:!   :!:     :!:            
-${bold}${lightblue} ::          ::     :: ::::  ::   :::  ::::: ::       ::::    :::     ::        
-${bold}${lightblue} :           :     : :: ::    :   : :   : :  :         :       :      :          
-                                                                                                  
-                                                                                                                
-${bold}${lightgreen}========================================================================
- "
- 
-echo "${nc}"
+    read -p "Ваш выбор [1-2]: " DISTRO_CHOICE
     
-    echo "${bold}${lightgreen}==> Started ${lightblue}Container${lightgreen} <=="
-    function runcmd1 {
-        printf "${bold}${lightgreen}Default${nc}@${lightblue}Container${nc}:~ "
-        read -r cmdtorun
-        ./libraries/proot -S . /bin/bash -c "$cmdtorun"
-        runcmd
-    }
-    function runcmd {
-        printf "${bold}${lightgreen}Default${nc}@${lightblue}Container${nc}:~ "
-        read -r cmdtorun
-        ./libraries/proot -S . /bin/bash -c "$cmdtorun"
-        runcmd1
-    }
-    runcmd
+    case $DISTRO_CHOICE in
+        1) # Пользователь выбрал Ubuntu
+            # --- Меню выбора версии Ubuntu ---
+
+            case "${ARCH}" in
+                "x86_64")
+                    URL_ARCH="amd64"
+                    ;;
+                "aarch64")
+                    URL_ARCH="arm64"
+                    ;;
+                *)
+                    # Фоллбэк, если архитектура другая
+                    URL_ARCH="${ARCH}"
+                    ;;
+            esac
+            echo ""
+            echo "Выберите версию Ubuntu LTS:"
+            echo "[1] Ubuntu 24.04 LTS (Noble Numbat) - LATEST"
+            echo "[2] Ubuntu 22.04 LTS (Jammy Jellyfish)"
+            echo "[3] Ubuntu 20.04 LTS (Focal Fossa)"
+            echo "[4] Ubuntu 18.04 LTS (Bionic Beaver)"
+            echo "[5] Ubuntu 16.04 LTS (Xenial Xerus)"
+            
+            read -p "Ваш выбор [1-5]: " UBUNTU_CHOICE
+
+            case $UBUNTU_CHOICE in
+                1)
+                    ROOTFS_URL="https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-${URL_ARCH}-root.tar.xz"
+                    ;;
+                2)
+                    ROOTFS_URL="https://cloud-images.ubuntu.com/jammy/current/ubuntu-jammy-cloudimg-${URL_ARCH}-root.tar.gz"
+                    ;;
+                3)
+                    ROOTFS_URL="https://cloud-images.ubuntu.com/focal/current/ubuntu-focal-cloudimg-${URL_ARCH}-root.tar.gz"
+                    ;;
+                4)
+                    ROOTFS_URL="https://cloud-images.ubuntu.com/bionic/current/ubuntu-bionic-cloudimg-${URL_ARCH}-root.tar.gz"
+                    ;;
+                5)
+                    ROOTFS_URL="https://cloud-images.ubuntu.com/xenial/current/ubuntu-xenial-cloudimg-${URL_ARCH}-root.tar.gz"
+                    ;;
+                *)
+                    echo "Ошибка: Неверный выбор версии Ubuntu."
+                    exit 1
+                    ;;
+            esac
+            ;;
+        2) # Пользователь выбрал Debian
+            echo ""
+            echo "Выберите версию Alpine:"
+            echo "[1] Alpine 3.20 - LATEST"
+            echo "[2] Alpine 3.19"
+            echo "[3] Alpine 3.18"
+            echo "[4] Alpine 3.17"
+            echo "[5] Alpine 3.14"
+            echo "[6] Alpine 3.11"
+            echo "[7] Alpine 3.8"
+            echo "[8] Alpine 3.5"
+            
+            read -p "Ваш выбор [1-5]: " ALPINE_CHOICE
+
+            case $ALPINE_CHOICE in
+                1)
+                    ROOTFS_URL="https://dl-cdn.alpinelinux.org/alpine/v3.22/releases/${ARCH}/alpine-minirootfs-3.22.0-${ARCH}.tar.gz"
+                    ;;
+                2)
+                    ROOTFS_URL="https://dl-cdn.alpinelinux.org/alpine/v3.19/releases/${ARCH}/alpine-minirootfs-3.19.7-${ARCH}.tar.gz"
+                    ;;
+                3)
+                    ROOTFS_URL="https://dl-cdn.alpinelinux.org/alpine/v3.18/releases/${ARCH}/alpine-minirootfs-3.18.9-${ARCH}.tar.gz"
+                    ;;
+                4)
+                    ROOTFS_URL="https://dl-cdn.alpinelinux.org/alpine/v3.17/releases/${ARCH}/alpine-minirootfs-3.17.9-${ARCH}.tar.gz"
+                    ;;
+                5)
+                    ROOTFS_URL="https://dl-cdn.alpinelinux.org/alpine/v3.14/releases/${ARCH}/alpine-minirootfs-3.14.10-${ARCH}.tar.gz"
+                    ;;
+                6)
+                    ROOTFS_URL="https://dl-cdn.alpinelinux.org/alpine/v3.11/releases/${ARCH}/alpine-minirootfs-3.11.13-${ARCH}.tar.gz"
+                    ;;
+                7)
+                    ROOTFS_URL="https://dl-cdn.alpinelinux.org/alpine/v3.8/releases/${ARCH}/alpine-minirootfs-3.8.5-${ARCH}.tar.gz"
+                    ;;
+                8)
+                    ROOTFS_URL="https://dl-cdn.alpinelinux.org/alpine/v3.5/releases/${ARCH}/alpine-minirootfs-3.5.3-${ARCH}.tar.gz"
+                    ;;
+                *)
+                    echo "Ошибка: Неверный выбор версии Ubuntu."
+                    exit 1
+                    ;;
+            esac
+            ;;
+            # Используем последнюю стабильную версию Debian (Bullseye)
+            ROOTFS_URL="https://dl-cdn.alpinelinux.org/alpine/v3.22/releases/x86_64/alpine-minirootfs-3.22.0-x86_64.tar.gz"
+            PLACEHOLDER_MSG="Начинается установка Debian 11 (Bullseye)... Пожалуйста, подождите."
+            ;;
+        *) # Неверный выбор
+            echo "Ошибка: Неверный выбор дистрибутива."
+            exit 1
+            ;;
+    esac
+
+    # --- Загрузка и распаковка выбранного дистрибутива ---
+    clear
+    echo "${PLACEHOLDER_MSG}"
+    echo "Загрузка корневой файловой системы... Это может занять некоторое время."
+    
+    # Скачиваем архив
+    if curl -L -o /tmp/rootfs.tar.gz "${ROOTFS_URL}"; then
+        echo "Загрузка завершена."
+    else
+        echo "Ошибка: Не удалось скачать корневую файловую систему. Проверьте URL и подключение к сети."
+        exit 1
+    fi
+    
+    # Распаковываем архив
+    echo "Распаковка... "
+    # Ubuntu использует .tar.gz, Debian LxC - .tar.xz. Эта проверка обрабатывает оба случая.
+    if tar -I 'gzip' -xf /tmp/rootfs.tar.gz -C $ROOTFS_DIR; then
+        echo "Распаковка завершена."
+    else
+        echo "Ошибка: Не удалось распаковать архив."
+        exit 1
+    fi
 fi
+
+################################
+# Package Installation & Setup #
+################################
+
+# Скачиваем дополнительные утилиты, если они еще не установлены.
+# Эта логика выполняется в том же блоке if, что и установка дистрибутива.
+if [ ! -e $ROOTFS_DIR/.installed ]; then
+    echo "Установка дополнительных утилит (proot)..."
+    
+    curl -Lo $ROOTFS_DIR/usr/local/bin/proot "https://github.com/proot-me/proot/releases/download/v${PROOT_VERSION:-5.4.0}/proot-v${PROOT_VERSION:-5.4.0}-${ARCH}-static"
+    chmod 755 $ROOTFS_DIR/usr/local/bin/proot
+fi
+
+# =========================================================================================
+# Финальная настройка и очистка
+# =========================================================================================
+if [ ! -e $ROOTFS_DIR/.installed ]; then
+    echo "Финальная настройка..."
+    # Добавляем DNS серверы в resolv.conf.
+    printf "nameserver 1.1.1.1\nnameserver 1.0.0.1" > ${ROOTFS_DIR}/etc/resolv.conf
+    
+    # Удаляем временные файлы
+    rm -f /tmp/rootfs.tar.gz
+    
+    # Создаем файл .installed, чтобы эта логика не выполнялась при следующих запусках.
+    touch $ROOTFS_DIR/.installed
+fi
+
+# Выводим приветственное сообщение
+clear && cat << EOF
+PteroVM is started!
+Вы вошли в окружение выбранного дистрибутива.
+Для установки пакетов используйте 'apt update && apt install ...'
+EOF
+
+###########################
+# Start PRoot environment #
+###########################
+
+# Эта команда запускает PRoot и "монтирует" важные директории
+# из хост-системы в нашу корневую файловую систему.
+$ROOTFS_DIR/usr/local/bin/proot \
+    --rootfs="${ROOTFS_DIR}" \
+    --link2symlink \
+    --kill-on-exit \
+    --root-id \
+    --cwd=/root \
+    --bind=/proc \
+    --bind=/dev \
+    --bind=/sys \
+    --bind=/tmp \
+    /bin/bash
---
+++ ptero-vm-original/README.md
@@ -0,0 +1,31 @@
+# :warning: Maintenance Notice
+
+Unfortunately, the original author, Afnanksalal, has abandoned and archived the project. The goal of this fork is to rewrite the project to optimize it and add more customization. You can find more information on why PteroVM is no longer being developed by reading this [comment](https://github.com/afnan007a/Ptero-vm/issues/32#issuecomment-1282276839).
+
+
+## Thank you:
+
+- loolkkekr - Rewrote PteroVM. (again)
+- Afnanksalal - Original Author PteroVM
+- Triassic - Made PteroVM Jar.
+- Chirag - Rewrote PteroVM.
+- Io.Netty - Original Idea of PteroVM
+
+## ✨ Note
+
+**Please use a host which have atleast 3GB disk space or it will messup the installation.**
+
+## Disclaimer
+
+This script is made for educational purposes (obviously lol, not like you're gonna abuse it)  
+We are NOT responsible for any consequences,  
+As stated in the LICENSE:
+```
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
